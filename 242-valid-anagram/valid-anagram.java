c7fca5ed4319a6c859098391874f07bca70851e0
PRAVEEN S <151344150+PRAVEEN1000-7@users.noreply.github.com>
2026-07-26 10:19:01 +0530

    Time: 3 ms (93.55%) | Memory: 44.8 MB (55.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,12 +3,24 @@ import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        if(s.length()!=t.length()) return false;
+
         char[] c1 = s.toCharArray();
         char[] c2 = t.toCharArray();
 
-        Arrays.sort(c1);
-        Arrays.sort(c2);
+        int[] arr = new int[26];
 
-        return Arrays.equals(c1, c2);
+        for(int i=0;i<c1.length;i++) {
+            arr[c1[i]-'a']++;
+            arr[c2[i]-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+
+        return true;    
     }
 }
